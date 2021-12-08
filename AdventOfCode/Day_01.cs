1d56f79e52d612db1b1807da09a107cc2f15d865
Emil Abrahamsson <emil.abrahamsson@lof.se>
2021-12-08 14:01:46 +0100

    Day1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -2,14 +2,38 @@
 
 public class Day_01 : BaseDay
 {
-    private readonly string _input;
+    private readonly int[] _input;
 
     public Day_01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToArray();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1(){      
+        var result = 0;
+        
+        for(int i = 1; i < _input.Length; ++i){
+            if(_input[i] > _input[i - 1]){
+                result++;
+            }
+        }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        return new(result.ToString());
+    }        
+
+    public override ValueTask<string> Solve_2(){
+        var result = 0;
+
+        for(int i = 1; i < _input.Length - 2; ++i){
+            if(GetThreeMeasurementSum(i) > GetThreeMeasurementSum(i - 1)){
+                result++;
+            }
+        }
+
+        return new(result.ToString());
+    }
+
+    private int GetThreeMeasurementSum(int baseIndex){
+        return _input[baseIndex] + _input[baseIndex + 1] + _input[baseIndex + 2];
+    }
 }
